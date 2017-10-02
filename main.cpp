5d43d7fc10d2e02fec1945913adea7e77add8b01
Ruslan Gulam <ruslan.gulam.n@gmail.com>
2017-10-02 19:18:30 +0300

    basic draw

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <ncurses.h>
+#include "snake.h"
 
 using namespace std;
 
--- snake.h
+++ snake.h
@@ -1,4 +1,11 @@
 #ifndef TEST_PROJECT_SNAKE_H
 #define TEST_PROJECT_SNAKE_H
 
+#include <iostream>
+#include <ncurses.h>
+#include <vector>
+#include <cstdlib>
+
+
+
 #endif //TEST_PROJECT_SNAKE_H
